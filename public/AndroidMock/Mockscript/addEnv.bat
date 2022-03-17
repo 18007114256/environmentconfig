19a877d832fb22c5e8a22b6bec918737360dbe22
18007114256 <18007114256@163.com>
2022-03-17 15:41:05 +0800

    新增环境脚本修改

--- public/AndroidMock/Mockscript/addEnv.bat
+++ public/AndroidMock/Mockscript/addEnv.bat
@@ -6,6 +6,7 @@ set envName=%3%
 rem 1、add
 echo "add env..."
 python add_env.py %envPath% %projectRootPath% %envName%
+python set_env.py %envPath% %projectRootPath% %envName%
 echo "add env end..."
 
 
