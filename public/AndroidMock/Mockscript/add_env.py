8418ea13a1eb6d2cd560df4badc42f984e84ca59
18007114256 <18007114256@163.com>
2022-03-17 15:19:01 +0800

    新增环境脚本修改

--- public/AndroidMock/Mockscript/add_env.py
+++ public/AndroidMock/Mockscript/add_env.py
@@ -73,9 +73,10 @@ def readfilePath(path):
     for file in files:  # 遍历文件夹
         if file != "main":  
             file_list.append(path + '\\'+file)
-    print(file_list)
+    # print(file_list)
     return file_list
 
+# 复制文件和目录
 def my_copy(path1,path2,type='file'):
     if type == 'file':
         shutil.copyfile(path1,path2)
@@ -98,8 +99,72 @@ lm_replace(new_Env_path_file, listEnv[len(listEnv) - 1].split("/")[1].split(".")
 # 复制src下环境目录，改名为新环境名
 listSrc =[]
 listSrc = readfilePath(src_path)
-print(listSrc)
+# print(listSrc)
 new_src_path_file = src_path + "\\" + sys.argv[3]
 my_copy(listSrc[len(listSrc) - 1], new_src_path_file,type='dir1')
 
+# 找到build.gradle文件
+buildList = []
+for root, dirs, files in os.walk(sys.argv[2]):
+    for name in files:
+        if name == "build.gradle":
+            buildList.append(os.path.join(root, name))
+            break
+# print(buildList)
+if(len(buildList) > 1) :
+    # 先修改项目build.gradle文件
+
+    #读取指定行：
+    lines=[]
+    f=open(buildList[0],'r', encoding='utf-8-sig')  #your path!
+    for line in f:
+        lines.append(line)
+    f.close()
+    # 写入apply gradle
+    data = "apply from: 'buildConfig/" + sys.argv[3] + ".gradle'\n"
+    lines.insert(5, data)           #插入并回车
+    # 写入新环境编译配置
+    lines.insert(len(lines) +2 ,"def  load"+ sys.argv[3] + "CompileSetting(){\n")                #插入并回车
+    lines.insert(len(lines) +2 ,"   loadGeneralCompileSetting()\n")               #插入并回车
+    lines.insert(len(lines) +2 ,"   project.ext.set(\""+ sys.argv[3] + "_ThirdSDKLib\", rootProject.ext."+ sys.argv[3] + "_ThirdSDKLib)\n")           #插入并回车
+    lines.insert(len(lines) +2 ,"   project.ext.set(\""+ sys.argv[3] + "_JarLib\", rootProject.ext."+ sys.argv[3] + "_JarLib)\n")           #插入并回车
+    lines.insert(len(lines) +2 ,"   project.ext.set(\""+ sys.argv[3] + "_CompileProject\", rootProject.ext."+ sys.argv[3] + "_CompileProject)\n")              #插入并回车
+    lines.insert(len(lines) +2 ,"   project.ext.set(\""+ sys.argv[3] + "_ThirdAarLib\", rootProject.ext."+ sys.argv[3] + "_ThirdAarLib)\n")           #插入并回车
+    lines.insert(len(lines) +2 ,"}\n")           #插入并回车
+
+    s=''.join(lines)
+    f=open(buildList[0],'w+', encoding='utf-8') #重新写入文件
+    f.write(s)
+    f.close()
+    del lines[:]                      #清空列表
+    # 再修改模块build.gradle文件
+    with open(buildList[1], "r", encoding="utf-8-sig") as f1:
+        fcontent = f1.readlines()
+        for line_num, line_content in enumerate(fcontent):
+            sub_str_index = line_content.find("productFlavors")
+            if sub_str_index != -1:
+                print("---在第{}行{}列".format(line_num, sub_str_index))
+                break 
+    #读取指定行：
+    lines=[]
+    f=open(buildList[1],'r', encoding='utf-8-sig')  #your path!
+    for line in f:
+        lines.append(line)
+    f.close()  
+    # 写入新环境编译配置
+    lines.insert(line_num +1 , "        "+ sys.argv[3] + " {\n")                #插入并回车
+    lines.insert(line_num +2  ,"            versionName rootProject.ext."+ sys.argv[3] + "_versionName\n")               #插入并回车
+    lines.insert(line_num +3  ,"            versionCode rootProject.ext."+ sys.argv[3] + "_versionCode\n")           #插入并回车
+    lines.insert(line_num +4  ,"            dimension rootProject.ext."+ sys.argv[3] + "_dimension\n")           #插入并回车
+    lines.insert(line_num +5  ,"            applicationId rootProject.ext."+ sys.argv[3] + "_applicationId\n")              #插入并回车
+    lines.insert(line_num +6  ,"            manifestPlaceholders = rootProject.ext."+ sys.argv[3] + "_manifestPlaceholders\n")           #插入并回车
+    lines.insert(line_num +7  ,"            load"+ sys.argv[3] + "CompileSetting()\n")           #插入并回车
+    lines.insert(line_num +8  ,"        }\n")           #插入并回车
+    s=''.join(lines)
+    f=open(buildList[1],'w+', encoding='utf-8') #重新写入文件
+    f.write(s)
+    f.close()
+    del lines[:]                      #清空列表
+else :
+    print("-Env- find build.gradle fail")
 print("---add env end---")
